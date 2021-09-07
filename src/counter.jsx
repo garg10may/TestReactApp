5ad9c27eb1a765b3e2ca8f3948d45e1e23a785b4
garg10may <garg10may@gmail.com>
2021-09-07 12:42:47 +0530

    change to SFC

--- src/counter.jsx
+++ src/counter.jsx
@@ -1,33 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class Counter extends Component {
-
-  render() {
-
-    const { onDelete, onReset, onIncrement, value, id } = this.props;
-
-    // here the click events are bubbled to the topmost 'App' component
-    // we pass the id, but then others are just references and finally we use the id
-    return (
-      <div>
-        <div style={{ margin: 10, fontWeight: 20, float: 'left' }}>{value}</div>
-        <button style={{ margin: 10 }} onClick={() => { onIncrement(id) }}>Increment</button>
-        <button style={{ margin: 10 }} onClick={this.onReset}>Reset</button>
-        <button style={{ margin: 10 }} onClick={this.onDelete}>Delete</button>
-      </div>
-    );
-  }
-
-  onDelete = () => {
-    this.props.onDelete(this.props.id);
-  }
-
-  onIncrement = () => {
-    this.props.onIncrement(this.props.id);
-  }
-
-  onReset = () => {
-    this.props.onReset(this.props.id);
-  }
-
+export default function Counter({ onDelete, onReset, onIncrement, value, id }) {
+  return (
+    <div>
+      <div style={{ margin: 10, fontWeight: 20, float: 'left' }}>{value}</div>
+      <button style={{ margin: 10 }} onClick={() => { onIncrement(id) }}>Increment</button>
+      <button style={{ margin: 10 }} onClick={() => { onReset(id) }}>Reset</button>
+      <button style={{ margin: 10 }} onClick={() => { onDelete(id) }}>Delete</button>
+    </div>
+  );
 }
