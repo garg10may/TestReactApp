e38985fa718e12f6f346ccf8f35f0c8a91784255
garg10may <garg10may@gmail.com>
2021-09-11 00:45:16 +0530

    remove uncesary files

--- src/reduxLearning.jsx
+++ src/reduxLearning.jsx
@@ -1,1 +1,19 @@
 
+//reducer is a pure function
+//In pure function we don't work with dom elements, do api call, use global variables
+
+function reducer (state=[], action) {
+  if (action.type ==='bugAdded') {
+    return [...state, { id: ++lastid}]
+  } else if (action.type==='bugRemoved') {
+
+  } else{
+    return state;
+  }
+}
+
+import { createStore} from 'redux';
+
+const store = createStore(reducer);
+export default store;
+
